4da07678c76d4822e21e66858c1c15a8db53cbae
Erelumnir <36478478+Erelumnir@users.noreply.github.com>
2018-09-19 12:47:03 +0200

    Update 2.0

---
+++ AAA/Assets/_scripts/HeartAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartAnimation : MonoBehaviour {
+
+	// Variables
+	public GameObject heartPE;
+	public int time;
+
+	public void PlayAnim()
+	{
+		GameObject heartInst = (GameObject)Instantiate(heartPE);
+		Destroy(heartInst, time);
+	}
+}
